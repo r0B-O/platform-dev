ffb8b5ae6acea338ace0cbbf123027e10678282f
kiran-balachandran <kiran.balachandran@allianz.com>
2024-04-16 01:22:50 +0530

    fix: file name typo

--- update-author.sh
+++ update-author.sh
@@ -7,9 +7,14 @@ echo "Author: $author"
 echo "$changed_files"
 
 for file in ${changed_files}; do
-  echo "Updating: $file"
-  sed -i "1s/^# Author:.*/# Author: ${author}\n/" ${file}
-  sed -i "1i# Author: ${author}\n" !~ /^# Author:/ ${file}
-  git add ${file}
-  git commit -m "Add author comment"
+    if [[ -f "$file" && "${file##*.}" =~ ^(yaml|yml)$i ]]; then
+        echo "Updating: $file"
+        sed -i "1s/^# Author:.*/# Author: ${author}\n/" ${file}
+        sed -i "1i# Author: ${author}\n" !~ /^# Author:/ ${file}
+        git add ${file}
+        git commit -m "Add author comment"
+  
+    else
+        echo "Skipping: '$file' is not a YAML file or does not exist."
+    fi
 done
