77383d90ef2a885a399e8bc708e3ae066e42ca94
kiran-balachandran <kiran.balachandran@allianz.com>
2024-04-16 01:10:14 +0530

    add more info to debug

--- update-author.sh
+++ update-author.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+set -x
 changed_files=$(gh pr view $PR_NUMBER --json files -q ".files[].path" | grep -v '^.github')
 author=$(git log -1 --pretty=%ae)
 
 echo "Author: $author"
 echo "$changed_files"
-
+pwd && ls -lart
 for file in ${changed_files}; do
   echo "Updating: $file"
   sed -i "1s/^# Author:.*/# Author: ${author}\n/" ${file}
