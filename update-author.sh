a519cd4263c1b7d7379135adb1507f673b48a908
kiran-balachandran <kiran.balachandran@allianz.com>
2024-04-16 12:36:26 +0530

    update jq parsing

--- update-author.sh
+++ update-author.sh
@@ -14,7 +14,7 @@ for file in ${changed_files}; do
         sed -i "1i# Author: ${author}\n" !~ /^# Author:/ ${file}
         git add ${file}
         git commit -m "chore: insert author name"
-        head_branch=$(gh pr view $PR_NUMBER --json headRefName | jq '.headRefName')
+        head_branch=$(gh pr view $PR_NUMBER --json headRefName | jq -r '.headRefName')
         git push origin $head_branch
     else
         echo "Skipping: '$file' is not a YAML file or does not exist."
