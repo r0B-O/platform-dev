dfd7c69635be85ea623cb4d025968fcdadf8938b
kiran-balachandran <kiran.balachandran@allianz.com>
2024-04-16 11:29:04 +0530

    fix push to head

--- update-author.sh
+++ update-author.sh
@@ -9,11 +9,12 @@ echo "$changed_files"
 for file in ${changed_files}; do
     if [[ -f "$file" && "${file##*.}" =~ ^(yaml|yml)$i ]]; then
         echo "Updating: $file"
-        sed -i "1s/^# Author:.*/# Author: ${author}\n/" ${file}
+        sed -i "1s/^# Author:.*/# Author: ${author}\n/" ${file} &&
         sed -i "1i# Author: ${author}\n" !~ /^# Author:/ ${file}
         git add ${file}
         git commit -m "Add author comment"
-        git push origin HEAD:$(git rev-parse --abbrev-ref HEAD)
+        head_branch=$(gh pr view $PR_NUMBER --json headRefName | jq '.headRefName')
+        git push origin $head_branch
     else
         echo "Skipping: '$file' is not a YAML file or does not exist."
     fi
