5297a819b4475e9d3323c61030c60553562f218a
kiran-balachandran <kiran.balachandran@allianz.com>
2024-04-16 12:22:56 +0530

    update workflow

--- update-author.sh
+++ update-author.sh
@@ -9,10 +9,11 @@ echo "$changed_files"
 for file in ${changed_files}; do
     if [[ -f "$file" && "${file##*.}" =~ ^(yaml|yml)$i ]]; then
         echo "Updating: $file"
+        pwd && ls -lrt
         sed -i "1s/^# Author:.*/# Author: ${author}\n/" ${file} &&
         sed -i "1i# Author: ${author}\n" !~ /^# Author:/ ${file}
         git add ${file}
-        git commit -m "Add author comment"
+        git commit -m "chore: insert author name"
         head_branch=$(gh pr view $PR_NUMBER --json headRefName | jq '.headRefName')
         git push origin $head_branch
     else
